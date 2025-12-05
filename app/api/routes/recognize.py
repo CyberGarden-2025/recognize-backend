2c5c356724b03a8a62146bdcb8b268b717357a74
Shoker2 <TheMallisteri@mail.ru>
2025-12-06 00:34:23 +0300

    feat: Загрузка фото в S3

--- app/api/routes/recognize.py
+++ app/api/routes/recognize.py
@@ -1,9 +1,16 @@
 import io
+import uuid
+import asyncio
+from pathlib import Path
+
 from fastapi import status, HTTPException, APIRouter, File, UploadFile
 from PIL import Image
+
 from app.services.llm_advice_picker import garbage_classifier
 from app.services.task_queue import task_manager, Task
+from app.services.s3_client import s3_client
 from app.api.schemas.recognize import RecognizeResponce
+from app.settings import SETTINGS
 
 MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB
 
@@ -14,16 +21,18 @@ router = APIRouter(prefix="/recognize", tags=["Recognize"])
 async def add_recognize_task(
     file: UploadFile = File(..., description="Изображение для распознавания")
 ) -> RecognizeResponce:
-    content = io.BytesIO(await file.read())
+    contents = io.BytesIO(await file.read())
 
-    if len(content.getvalue()) > MAX_FILE_SIZE:
+    # Проверка на размер
+    if len(contents.getvalue()) > MAX_FILE_SIZE:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Файл слишком большой. Максимальный размер: {MAX_FILE_SIZE / 1024 / 1024}MB",
         )
 
+    # Проверка на корректность фото
     try:
-        image = Image.open(content)
+        image = Image.open(contents)
         image.verify()
     except Exception as img_error:
         raise HTTPException(
@@ -31,7 +40,21 @@ async def add_recognize_task(
             detail=f"Некорректный файл изображения: {str(img_error)}",
         )
 
-    task = task_manager.add_task(garbage_classifier.classify(content.getvalue()))
+    image_data = contents.getvalue()
+
+    # Задача на обработку фото
+    task = task_manager.add_task(garbage_classifier.classify(image_data))
+
+    # Сохранение фото в S3
+    ext = Path(file.filename).suffix
+    asyncio.create_task(
+        s3_client.upload_bytes(
+            data=image_data,
+            bucket_name=SETTINGS.S3_BUCKET_NAME,
+            object_name=f"{uuid.uuid4().hex}{ext}",
+        )
+    )
+
     return RecognizeResponce(task_id=task.id)
 
 
