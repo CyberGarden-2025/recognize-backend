05b2abca49f89a0e9d4767252a9a6367b5ecacf0
Shoker2 <TheMallisteri@mail.ru>
2025-12-06 00:04:50 +0300

    chore(Settings): Стандартные значения

--- app/settings.py
+++ app/settings.py
@@ -8,10 +8,10 @@ load_dotenv(".env")
 
 
 class Settings(BaseModel):
-    IS_DEBUG: bool
-    LOGGING_LEVEL: str
-    API_HOST: str
-    API_PORT: int
+    IS_DEBUG: bool = False
+    LOGGING_LEVEL: str = "INFO"
+    API_HOST: str = "0.0.0.0"
+    API_PORT: int = "8000"
 
     HTTP_PROXY_SERVER: Optional[SecretStr] = None
 
@@ -19,10 +19,10 @@ class Settings(BaseModel):
     OPENAI_API_KEY: SecretStr
     OPENAI_GPT_MODEL: str
 
-    S3_ENDPOINT: Optional[str]
+    S3_ENDPOINT: Optional[str] = None
     S3_ACCESS_KEY: Optional[SecretStr] = None
     S3_SECRET_KEY: Optional[SecretStr] = None
-    S3_BUCKET_NAME: Optional[str]
+    S3_BUCKET_NAME: Optional[str] = None
 
 
 SETTINGS = Settings(**environ)
